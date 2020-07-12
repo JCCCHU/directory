6d8917a2ff79559967dc2be8b841d14995ee04ab
Joshua Chu <jashya@gmail.com>
2020-07-12 17:09:44 -0400

    Initialize with friends app, replace friends with employees

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ employees });
+  };
+
+  sortEmployee = sort => {
+    const employees = this.state.employees.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    this.setState({ employees });
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee List</Title>
+        <button onClick={this.sortEmployee}>Sort employees</button>
+        {
+        this.state.employees.map(employee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            /* image={friend.image}*/
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
